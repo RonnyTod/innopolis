b9d78328b04c88f4a163c0fc1d90192e54abb90f
Ilya Bosykh <viewtohell@gmail.com>
2021-01-12 18:49:38 +0300

    Main view component and his usage in App.vue

---
+++ src/views/MainView.vue
@@ -0,0 +1,27 @@
+<template>
+  <ValidateComponent id="1">
+    <SomeComponent>
+      <ValidateComponent id="2"></ValidateComponent>
+    </SomeComponent>
+    <SomeComponent>
+      <ValidateComponent id="3">
+        <ValidateComponent id="4"></ValidateComponent>
+      </ValidateComponent>
+    </SomeComponent>
+    <ValidateComponent id="5"></ValidateComponent>
+  </ValidateComponent>
+</template>
+
+<script>
+  import SomeComponent from "@/components/common/SomeComponent";
+  import ValidateComponent from "@/components/common/ValidateComponent";
+
+  export default {
+    name: "MainView",
+    components: { SomeComponent, ValidateComponent }
+  };
+</script>
+
+<style scoped>
+
+</style>
