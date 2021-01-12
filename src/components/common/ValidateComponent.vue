6c195ffa59d614a13cc495a40e9f47d711984d9e
Ilya Bosykh <viewtohell@gmail.com>
2021-01-12 18:52:41 +0300

    Main component booklet component. Result of execution will be showed in console.

---
+++ src/components/common/ValidateComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ValidateComponent",
+    props: {
+      id: {
+        type: String,
+        required: true
+      }
+    },
+    provide () {
+      return {
+        parent: this
+      };
+    },
+    inject: {
+      parent: { default: undefined }
+    },
+    mounted () {
+      if (this.parent?.$options.name === this.$options.name) {
+        this.validate(this.parent, this);
+      }
+    },
+    methods: {
+      validate: (caller, called) => {
+        console.log(`id ${ caller.id }`, `called id ${ called.id }`);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
